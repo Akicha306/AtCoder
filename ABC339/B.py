65b03386d6291b54c5f786a0ba9a5e79839e0de9
akicha <akicha306@gmail.com>
2024-05-26 16:52:09 +0900

    339

--- ABC339/B.py
+++ ABC339/B.py
@@ -2,7 +2,36 @@ H,W,N = list(map(int, input().split()))
 
 field = [["."]*W for _ in range(H) ]
 x,y = 0,0
+direction = 0
 
+direction_list = [(0,-1),(1,0),(0,1),(-1,0)] 
 
 while N > 0:
+    if field[y][x] == ".":
+        field[y][x] = "#"
+        direction += 1
+        if direction > 3:
+            direction = 0
+        
+    else:
+        field[y][x] = "."
+        direction -= 1
+        if direction < 0:
+            direction = 3
     
+    x,y = x + direction_list[direction][0], y + direction_list[direction][1]
+    
+    if x < 0:
+        x = W- 1
+    elif x >= W:
+        x = 0
+        
+    if y < 0:
+        y = H - 1
+    elif y >= H:
+        y = 0
+        
+    N -= 1
+    
+for i in range(H):
+    print("".join(field[i]))
--- ABC339/C.py
+++ ABC339/C.py
@@ -1,1 +1,10 @@
+N =input()
+A = list(map(int, input().split()))
+answer = 0
 
+for i in A:
+    answer += i
+    if answer <0:
+        answer = 0
+    
+print(answer)
